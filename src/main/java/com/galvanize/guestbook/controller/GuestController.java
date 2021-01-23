70829f59f0b559a6fd5d66fc0ab4c734e6f8ff9b
AsiyatK <aliciabostan@gmail.com>
2021-01-23 13:39:59 -0500

    Added post method for adding new guest entries

--- src/main/java/com/galvanize/guestbook/controller/GuestController.java
+++ src/main/java/com/galvanize/guestbook/controller/GuestController.java
@@ -1,20 +1,29 @@
 package com.galvanize.guestbook.controller;
 
 import com.galvanize.guestbook.bean.GuestEntry;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import javax.jws.HandlerChain;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class GuestController {
 
-    private List guestList =  new ArrayList();
+    private List<GuestEntry> guestList =  new ArrayList();
 
     @GetMapping("/entries")
     public List getAllGuestEntries(){
         guestList.add(new GuestEntry(1,"Abaram Lincoln","Awesome site"));
         return guestList;
+    }
+
+
+    @PostMapping("/entries")
+    @ResponseStatus(HttpStatus.CREATED)
+    public GuestEntry createEntry(@RequestBody GuestEntry guestEntry){
+        guestList.add(guestEntry);
+        return guestList.get(guestList.size()-1);
     }
 }
--- src/test/java/com/galvanize/guestbook/GuestbookApplicationTest.java
+++ src/test/java/com/galvanize/guestbook/GuestbookApplicationTest.java
@@ -1,8 +1,13 @@
 package com.galvanize.guestbook;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.galvanize.guestbook.bean.GuestEntry;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -14,8 +19,22 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest
 public class GuestbookApplicationTest {
 
+
+
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper mapper;
+    GuestEntry request = new GuestEntry();
+
+    @BeforeEach
+    public void init(){
+        request.setId(1);
+        request.setName("Marie");
+        request.setComments("Science rocks!");
+    }
+
 
     @Test
     void test_getAllGuestBookEntries_returnList() throws Exception {
@@ -23,6 +42,16 @@ public class GuestbookApplicationTest {
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(jsonPath("$.length()").value(1))
                 .andExpect(jsonPath("$.[0].name").value("Abaram Lincoln"));
+
+    }
+
+    @Test
+    void test_addEntry_returnsNewEntry() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/entries")
+                .content(mapper.writeValueAsString(request))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(jsonPath("$.name").value("Marie"));
 
     }
 }
